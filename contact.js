1e2889119ab7630d8fcde5925c8e4c2c8c822193
unknown <roseph@SL-337-16.CISAT.JMU.EDU>
2013-02-14 20:20:17 -0500

    Added exam to repo.

---
+++ contact.js
@@ -0,0 +1,15 @@
+function checkContact()
+{ 
+	var street1 = document.contact.street.value;
+	var street2 = document.contact.street.value;
+	var city = document.contact.city.value;
+	var state = document.contact.state.value;
+	var zip = document.contact.zip.value;
+	var phone = document.contact.phone.value;
+	var email = document.contact.email.value;
+	
+	if(street1 == "")
+	{
+		alert("Please enter an address.")
+	)
+}	
